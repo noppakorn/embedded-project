7da737a073604f97140b2f85d409b8648dd5c509
Noppakorn Jiravaranun <noppakorn@noppakorn.com>
2022-04-18 23:37:04 +0700

    update ui for student count

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -9,6 +9,7 @@ import { useEffect, useState } from "react";
 import firebase from "../lib/firebase";
 
 const Index = () => {
+  const classroomCapacity = 50;
   const [students, setStudents] = useState<DocumentData[]>([]);
   useEffect(() => {
     const db = getFirestore(firebase);
@@ -22,31 +23,35 @@ const Index = () => {
     });
     return unsubscribe;
   }, []);
-  console.log(students.length);
   return (
     <div className="font-sans flex flex-col justify-center w-full p-16">
       <div className="text-center text-3xl font-bold">
         Student Checked in to Classroom #1
       </div>
-      <div className="text-center text-3xl">student count: {students.length} </div>
+      <div className="flex flex-row border justify-center items-center text-center text-xl mt-5 p-5">
+        <span className="font-bold">Students in classroom :</span>&nbsp;
+        <span>
+          {students.length}/{classroomCapacity}
+        </span>
+      </div>
       <div className="flex flex-col justify-center w-full">
         <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-10 justify-center items-center border p-5">
           {students.map((doc) => {
             return (
               <div
                 key={doc.data().first_name}
-                className="flex flex-col items-center justify-between text-center border h-full break-all p-5 hover:bg-gray-100"
+                className="flex flex-col items-center justify-between text-center border h-full break-words p-5 hover:bg-gray-100 duration-300"
               >
                 <div className="text-xl font-bold">
                   {doc.data().first_name} {doc.data().last_name}
                 </div>
                 <button
-                  className="border mt-2 p-1.5 hover:bg-red-200 hover:underline"
+                  className="border mt-2 p-2 hover:bg-red-300 hover:underline active:bg-red-400 duration-500 "
                   onClick={async () => {
                     await deleteDoc(doc.ref);
                   }}
                 >
-                  Check out
+                  Check Out
                 </button>
               </div>
             );
