27b8580261fa50d41fad0b79abe2fd585d217092
nopparuj917 <nopparuj.ta@gmail.com>
2022-04-19 12:20:07 +0700

    filter by search word

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,5 @@
 import {
+  arrayUnion,
   collection,
   deleteDoc,
   DocumentData,
@@ -10,6 +11,23 @@ import firebase from "../lib/firebase";
 
 const Index = () => {
   const [students, setStudents] = useState<DocumentData[]>([]);
+  const [wordEntered,setWordEntered] = useState("");
+  const [filteredData,setFilterData] = useState<DocumentData[]>([]);
+  const handleInput = (event)=>{
+    const db = getFirestore(firebase);
+    const studentsRef = collection(db, "students-name");
+    const searchWord = event.target.value;
+    setWordEntered(searchWord);
+    const newFilter = onSnapshot(studentsRef ,(query)=>{
+      let arr: DocumentData[] = [];
+      query.forEach((a) => {
+        if(a.data().first_name.toLowerCase().includes(searchWord.toLowerCase())){
+          arr.push(a);
+        }
+      });
+      setFilterData(arr);
+    });
+  }
   useEffect(() => {
     const db = getFirestore(firebase);
     const studentsRef = collection(db, "students-name");
@@ -23,32 +41,32 @@ const Index = () => {
     return unsubscribe;
   }, []);
   return (
-    <div className="font-sans flex flex-col justify-center w-full p-16">
+    <div className="font-sans flex flex-col justify-center w-full p-16 space-y-4">
       <div className="text-center text-3xl font-bold">
         Student Checked in to Classroom #1
       </div>
       <div className="text-center text-3xl">
-        student count: {students.length}{" "}
+        student count: {students.length}
       </div>
       <div className="flex justify-center">
         <div className="mb-3"></div>
         <input
           type="search"
-          className="form-control block w-full px-3 py-1.5 text-base font-normal
+          className="form-control block w-fit px-3 py-1.5 
+                text-base font-normal
                 text-gray-700
                 bg-white bg-clip-padding
                 border border-solid border-gray-300
-                rounded
-                transition
-                ease-in-out
-                m-0"
-          id="exampleSearch"
-          placeholder="Type query"
+                rounded transition ease-in-out m-0"
+          id="Search"
+          placeholder="Search"
+          value={wordEntered}
+          onChange={handleInput}
         ></input>
       </div>
       <div className="flex flex-col justify-center w-full">
         <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-10 justify-center items-center border p-5">
-          {students.map((doc) => {
+          {filteredData.map((doc) => {
             return (
               <div
                 key={doc.data().first_name}
