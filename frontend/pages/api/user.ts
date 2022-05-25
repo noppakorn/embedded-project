19ea7df775cc1102e13818edd46e2357460a192a
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2022-05-25 14:56:48 +0700

    get request

--- frontend/pages/api/user.ts
+++ frontend/pages/api/user.ts
@@ -6,6 +6,7 @@ import {
   collection,
   setDoc,
   getDoc,
+  QuerySnapshot,
 } from "firebase/firestore";
 
 export default async function handler(
@@ -23,16 +24,32 @@ export default async function handler(
         res.status(404).end("Not found");
       }
     } else if (typeof req.query.card_id === "string") {
-      res.status(200).json({
-        status: "OK",
-        receivedData: req.query.card_id,
-      });
+      // res.status(200).json({
+      //   status: "OK",
+      //   receivedData: req.query.card_id,
+      // });
+      const roomRef = doc(db, "room", req.query.card_id);
+      const query = await getDoc(roomRef);
+      const card = await getDoc(doc(db, "card-id", req.query.card_id));
+      if(query.exists()) {
+        res.status(200).json(card.data());
+      }
+      // else if(!query.exists()) {
+        
+      // }
     } else {
       res.status(500).end();
     }
   } else if (req.method === "POST") {
-    const studentsRef = collection(db, "students-name");
-    await setDoc(doc(studentsRef, req.body.student_id), {
+    // const studentsRef = collection(db, "students-name");
+    // await setDoc(doc(studentsRef, req.body.student_id), {
+    //   first_name: req.body.first_name,
+    //   last_name: req.body.last_name,
+    // }).then(() => {
+    //   res.status(200).end();
+    // });
+    const roomRef = collection(db, "room");
+    await setDoc(doc(roomRef, req.body.card_id), {
       first_name: req.body.first_name,
       last_name: req.body.last_name,
     }).then(() => {
