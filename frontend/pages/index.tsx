e60ec8d3852d1c616f838548d0598974327b41fd
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2022-05-26 00:55:25 +0700

    edit check in time message

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -78,6 +78,20 @@ const Index = () => {
       <div className="flex flex-col justify-center w-full">
         <div className="grid sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-10 justify-center items-center border p-5">
           {filteredStudents.map((doc) => {
+            let checkinTime = Math.floor(((time - new Date(doc.data().timestamp.seconds * 1000).getTime()) / 1000) / 60);
+            let timeMessage = "";
+            if(checkinTime <= 1) {
+              timeMessage = "check in a minute ago"
+            }
+            else if(checkinTime > 1 && checkinTime < 60) {
+              timeMessage = "check in " + checkinTime + " minutes ago" 
+            }
+            else if(checkinTime >= 60 && checkinTime < 120) {
+              timeMessage = "check in 1 hour ago" 
+            }
+            else {
+              timeMessage = "check in " + Math.floor(checkinTime / 60) + " hours ago" 
+            }
             return (
               <div
                 key={doc.id}
@@ -87,7 +101,7 @@ const Index = () => {
                   {doc.data().first_name} {doc.data().last_name}
                 </div>
                 <div>
-                  checked in {Math.floor(((time - new Date(doc.data().timestamp.seconds*1000).getTime()) / 1000) / 60)} minutes ago
+                  {timeMessage}
                 </div>
                 <button
                   className="border mt-2 p-2 hover:bg-red-300 hover:underline active:bg-red-400 duration-500 "
